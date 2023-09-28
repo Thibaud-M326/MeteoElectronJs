5c4d0174676ed2973bd3c3f7b5e4db58af1fade4
francois <francoismazerbourg@gmail.com>
2023-09-28 16:44:59 +0200

    front

--- weather.js
+++ weather.js
@@ -1,10 +1,12 @@
 import { getCoordsAPI, getWeatherAPI } from "./api.js";
 
 document.getElementById('searchForm').addEventListener('submit',async function(event) {
-    event.preventDefault(); // Empêche la soumission du formulaire par défaut
+    event.preventDefault();
 
     const searchTerm = document.getElementById('search').value;
 
     const {lat, lon} = await getCoordsAPI(searchTerm);
     console.log(await getWeatherAPI(lat, lon));
+
+    const dataApi = document.getElementById('dataApi');
 });
