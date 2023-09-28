adacd64d16486bc445d0411cbda36edeb9999946
francois <francoismazerbourg@gmail.com>
2023-09-28 18:08:59 +0200

    front almost done data displayed

--- weather.js
+++ weather.js
@@ -8,5 +8,17 @@ document.getElementById('searchForm').addEventListener('submit',async function(e
     const {lat, lon} = await getCoordsAPI(searchTerm);
     console.log(await getWeatherAPI(lat, lon));
 
-    const dataApi = document.getElementById('dataApi');
+    const data = await getWeatherAPI(lat, lon); 
+
+    var dataApi = document.getElementById("dataApi");
+    dataApi.style.display = "block";
+
+    console.log(data.main.temp);
+
+    console.log("https://openweathermap.org/img/wn/" + data.weather[0].icon + "@4x.png");
+    document.getElementById('imgWeather').src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@4x.png";
+    document.getElementById('numberDegre').innerHTML= Math.round(data.main.temp - 273.15);
+    document.getElementById('weather').innerHTML= data.weather[0].description;
+    document.getElementById('humidityNumber').innerHTML= data.main.humidity;
+    document.getElementById('windSpeed').innerHTML= data.wind.speed;
 });
